a8b4ba5c299ec7b5e1c44b78e94d48a4662afe35
adrichaigneau <126883920+adrichaigneau@users.noreply.github.com>
2023-06-12 16:36:37 +0200

    Delete dist/out-tsc directory

--- dist/out-tsc/app/app/safe-pipe.pipe.spec.js
+++
@@ -1,8 +0,0 @@
-import { SafePipePipe } from './safe-pipe.pipe';
-describe('SafePipePipe', () => {
-    it('create an instance', () => {
-        const pipe = new SafePipePipe();
-        expect(pipe).toBeTruthy();
-    });
-});
-//# sourceMappingURL=safe-pipe.pipe.spec.js.map
